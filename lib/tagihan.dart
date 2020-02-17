90ab36e557e3e3d661a18b6575222221cdf897ea
Sigit Prasetyo Noprianto <noprisigit@gmail.com>
2020-02-17 12:00:08 +0700

    menambahkan gesture detector di card

--- lib/tagihan.dart
+++ lib/tagihan.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
 import 'package:tp2/dashboard.dart';
 import 'package:tp2/utils/colors_utils.dart';
 import 'package:http/http.dart' as http;
@@ -14,6 +15,7 @@ class Tagihan extends StatefulWidget {
 }
 
 class _TagihanState extends State<Tagihan> {
+  ScrollController _scrollController;
 
   Map data;
   List listData;
@@ -22,11 +24,10 @@ class _TagihanState extends State<Tagihan> {
   bool _issetData = false;
 
   Future getData(String value) async {
-    
-    String url = "http://e-water.systems/adfin_pdam/public/api/v1/finance/tagihanAll/";
-    var response = await http.get(url + value, headers: {
-      'Accept' : 'application/json'
-    });
+    String url =
+        "http://e-water.systems/adfin_pdam/public/api/v1/finance/tagihanAll/";
+    var response =
+        await http.get(url + value, headers: {'Accept': 'application/json'});
     data = json.decode(response.body);
     setState(() {
       _issetData = true;
@@ -37,85 +38,80 @@ class _TagihanState extends State<Tagihan> {
   }
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    _scrollController = ScrollController();
+    super.initState();
+  }
 
+  @override
+  Widget build(BuildContext context) {
     TextEditingController _formTagihan = new TextEditingController();
 
     final label = Padding(
-      padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0),
-      child: Text(
-        "Kode Online Pelanggan",
-        style: TextStyle(
-          fontWeight: FontWeight.w400,
-          fontSize: 16.0
-        ),
-      )
-    );
+        padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0),
+        child: Text(
+          "Kode Online Pelanggan",
+          style: TextStyle(fontWeight: FontWeight.w400, fontSize: 16.0),
+        ));
 
     final formTagihan = Padding(
-      padding: EdgeInsets.all(10.0),
+        padding: EdgeInsets.all(10.0),
         child: TextField(
-        controller: _formTagihan,
-        keyboardType: TextInputType.number,
-        style: TextStyle(
-          fontSize: 16.0
-        ),
-        decoration: InputDecoration(
-          hintText: "Kode Online Pelanggan",
-          enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.blue )),
-          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: HexColor('#1ab394')))
-        ),
-      )
-    );
+          controller: _formTagihan,
+          keyboardType: TextInputType.number,
+          style: TextStyle(fontSize: 16.0),
+          decoration: InputDecoration(
+              hintText: "Kode Online Pelanggan",
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue)),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: HexColor('#1ab394')))),
+        ));
 
     final button = Center(
-      child: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Material(
-          borderRadius: BorderRadius.circular(10.0),
-          shadowColor: Colors.black38,
-          elevation: 5.0,
-          child: MaterialButton(
-            onPressed: () {
-              setState(() {
-                _loading = true;
-              });
-              getData(_formTagihan.text);
-            },
-            child: Text(
-              "Submit",
-              style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'Nunito',
-                fontWeight: FontWeight.w700
+        child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Material(
+              borderRadius: BorderRadius.circular(10.0),
+              shadowColor: Colors.black38,
+              elevation: 5.0,
+              child: MaterialButton(
+                onPressed: () {
+                  setState(() {
+                    _loading = true;
+                  });
+                  getData(_formTagihan.text);
+                },
+                child: Text(
+                  "Submit",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Nunito',
+                      fontWeight: FontWeight.w700),
+                ),
+                minWidth: 200.0,
+                height: 50.0,
+                color: HexColor('#1ab394'),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0)),
               ),
-            ),
-            minWidth: 200.0,
-            height: 50.0,
-            color: HexColor('#1ab394'),
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
-          ),
-        )
-      )
-    );
+            )));
 
-    final listPelangganNotFound = ListView(
-      children: <Widget>[
-        _loading ? Center(child: CircularProgressIndicator()) : Card(
+    final listPelangganNotFound = ListView(children: <Widget>[
+      _loading ? Center(child: CircularProgressIndicator()) : Card(
           child: Padding(
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
                 "Data Tagihan Tidak Ditemukan / Tagihan Sudah Dibayar",
-                style: TextStyle(
-                  fontSize: 14.0,
-                  fontWeight: FontWeight.w700
-                ),textAlign: TextAlign.center,
+                style:
+                    TextStyle(fontSize: 14.0, fontWeight: FontWeight.w700),
+                textAlign: TextAlign.center,
               ),
             ),
           ),
         )
-      ],
+      ], 
       shrinkWrap: true
     );
 
@@ -125,64 +121,73 @@ class _TagihanState extends State<Tagihan> {
         children: <Widget>[
           Padding(
             padding: EdgeInsets.all(10.0),
-            child: Text("Nama Pelanggan : ${listData[0]['nama_pelanggan']}")
+            child: Text(
+              "Nama Pelanggan : ${listData[0]['nama_pelanggan']}",
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w500
+              ),
+            )
           ),
           Padding(
             padding: EdgeInsets.all(5.0),
             child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              controller: _scrollController,
               shrinkWrap: true,
               itemCount: listData == null ? 0 : listData.length,
               itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(15.0, 15.0, 0, 15.0),
-                    child:Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            Text("Bulan / Tahun"),
-                            Text(" : "),
-                            Text("${listData[index]['bulan']} / ${listData[index]['tahun']}")
-                          ],
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Text("Tarif Air           "),
-                            Text(" : "),
-                            Text("${listData[index]['tarif_air']}")
-                          ],
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Text("Tarif Non Air   "),
-                            Text(" : "),
-                            Text("${listData[index]['tarif_nonair']}")
-                          ],
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Text("Denda              "),
-                            Text(" : "),
-                            Text("${listData[index]['denda']}")
-                          ],
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Text("Total Tagihan "),
-                            Text(" : "),
-                            Text("${listData[index]['total_tagihan']}")
-                          ],
-                        ),
-                        // Text("Bulan              : ${listData[index]['bulan']}"),
-                        // Text("Tahun             : ${listData[index]['tahun']}"),
-                        // Text("Tarif Air          : ${listData[index]['tarif_air']}"),
-                        // Text("Tarif Non Air   : ${listData[index]['tarif_air']}"),
-                        // Text("Denda              : ${listData[index]['tarif_air']}"),
-                        // Text("Total Tagihan : ${listData[index]['tarif_air']}"),
-                      ],
-                    )
-                  ),
+                return GestureDetector(
+                  onTap: () {
+                   
+                  },
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(15.0, 15.0, 0, 15.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Text("Bulan / Tahun"),
+                              Text(" : "),
+                              Text(
+                                  "${listData[index]['bulan']} / ${listData[index]['tahun']}")
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Text("Tarif Air           "),
+                              Text(" : "),
+                              Text("${listData[index]['tarif_air']}")
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Text("Tarif Non Air   "),
+                              Text(" : "),
+                              Text("${listData[index]['tarif_nonair']}")
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Text("Denda              "),
+                              Text(" : "),
+                              Text("${listData[index]['denda']}")
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Text("Total Tagihan "),
+                              Text(" : "),
+                              Text(
+                                  "${listData[index]['total_tagihan']}")
+                            ],
+                          ),
+                        ],
+                      )
+                    ),
+                  )
                 );
               }
             ),
@@ -195,11 +200,13 @@ class _TagihanState extends State<Tagihan> {
       appBar: AppBar(
         title: Center(child: Text("List Tagihan")),
         leading: new IconButton(
-          icon: new Icon(Icons.arrow_back),
-          onPressed: () => Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => Dashboard() ), (Route<dynamic> route) => false)
-        ),
+            icon: new Icon(Icons.arrow_back),
+            onPressed: () => Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(
+                    builder: (BuildContext context) => Dashboard()),
+                (Route<dynamic> route) => false)),
       ),
-      body: Container(     
+      body: Container(
         child: ListView(
           children: <Widget>[
             label,
